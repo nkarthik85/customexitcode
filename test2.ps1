c170d108edca18b9e3ca77e902cb0936a56a0c43
nkarthik85 <133115940+nkarthik85@users.noreply.github.com>
2023-05-30 23:56:29 +0530

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,12 +1,5 @@
 #param ($input1, $input2)
-$isNew = $true1
-$isOld = $false2
-$outputs = @{
-    AliasName = test
-    IsNew = $isNew
-    IsOld = $isOld
-}
-Write-Output -InputObject $outputs
-#Write-Host "$input1 $input2"
+$isNew = $true
+$isOld = $false
 Write-Host "$isNew"
 Write-Host "$isOld"
