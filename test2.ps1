34fec6d33e7b819944d083432bbe5a0377cf998b
nkarthik85 <133115940+nkarthik85@users.noreply.github.com>
2023-05-30 17:30:42 +0530

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,2 +1,9 @@
 param ($input1, $input2)
+$outputs = @{
+    AliasName = $AliasName
+    IndexName = $result.Value.Name
+    IndexVersion = $result.Value.Version
+    IsNew = $isNew
+}
+Write-Output -InputObject $outputs
 Write-Host "$input1 $input2"
